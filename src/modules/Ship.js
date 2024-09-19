c65b66f7dc04a63cb81c48e9c0fb89ef39f87618
rib9985 <rib9985@gmail.com>
2024-09-19 10:06:05 -0300

    Create ship class with initial methods

--- src/modules/Ship.js
+++ src/modules/Ship.js
@@ -1,7 +1,19 @@
 export default class Ship {
-  constructor(length, hits, sunk) {
+  constructor(length, hits = 0, sunk = false, vertical = false) {
     this.length = length;
     this.hits = hits;
     this.sunk = sunk;
+  }
+
+  isSunk() {
+    if (this.length === this.hits) {
+      this.sunk = true;
+    } else {
+      return;
+    }
+  }
+
+  hits() {
+    this.hits += 1;
   }
 }

bb9b867a0f333fc3397bd2e58f699c4fbe2d00f1
rib9985 <rib9985@gmail.com>
2024-09-24 18:10:29 -0300

    Add testing for checking bounds

--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -4,35 +4,49 @@ export default class Gameboard {
   constructor(rows = 10, columns = 10, isVertical = false) {
     this.boardSize = [rows, columns];
     this.board = this.generateBoardArray(rows, columns);
-    this.ships = this.shipFleet();
     this.isVerticalPlace = isVertical;
+    this.availableShips = this.shipFleet();
+    this.placedShips = [];
+    this.sunkShips = [];
   }
 
   generateBoardArray(rows, columns) {
     return Array.from({ length: rows }, () => new Array(columns).fill(0));
   }
 
+  shipFleet() {
+    const carrier = new Ship("carrier", 4);
+    const battleshipOne = new Ship("battleshipOne", 3);
+    const battleshipTwo = new Ship("battleshipTwo", 3);
+    const cruiserOne = new Ship("cruiserOne", 2);
+    const cruiserTwo = new Ship("cruiserTwo", 2);
+    const cruiserThree = new Ship("cruiserThree", 2);
+    const submarineOne = new Ship("submarineOne", 1);
+    const submarineTwo = new Ship("submarineTwo", 1);
+    const submarineThree = new Ship("submarineThree", 1);
+    const submarineFour = new Ship("submarineFour", 1);
+
+    const fleet = [
+      carrier,
+      battleshipOne,
+      battleshipTwo,
+      cruiserOne,
+      cruiserTwo,
+      cruiserThree,
+      submarineOne,
+      submarineTwo,
+      submarineThree,
+      submarineFour,
+    ];
+
+    return fleet;
+  }
+
   resetBoard() {
     this.board = this.generateBoardArray(this.boardSize[0], this.boardSize[1]);
-    this.ships = this.shipFleet();
-  }
-
-  checkWithinBoundsX(shipLength) {
-    const finalLength = length + this.board[0];
-    if (finalLength > this.x.length || finalLength < 0) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-
-  checkWithinBoundsY(shipLength) {
-    const finalLength = length + this.board[1];
-    if (finalLength > this.y.length || finalLength < 0) {
-      return false;
-    } else {
-      return true;
-    }
+    this.availableShips = this.shipFleet();
+    this.placedShips = [];
+    this.sunkShips = [];
   }
 
   checkIfPositionEmpty(row, column) {
@@ -46,75 +60,107 @@ export default class Gameboard {
     }
   }
 
-  shipFleet() {
-    const carrier = new Ship("carrier", 4);
-    const battleship = new Ship("battleship", 3);
-    const cruiser = new Ship("cruiser", 2);
-    const submarine = new Ship("submarine", 1);
-
-    const fleet = [
-      carrier,
-      battleship,
-      battleship,
-      cruiser,
-      cruiser,
-      cruiser,
-      submarine,
-      submarine,
-      submarine,
-      submarine,
-    ];
-
-    return fleet;
-  }
-
-  getShip(ship) {
-    let shipReturn = null;
-    const shipFound = this.ships.find((element) => {
-      element.shipClass == ship;
-    });
-    if (shipFound) {
-      shipReturn = shipFound;
-      const index = this.ships.indexOf(ship);
-      this.ships[index] = 0;
-    }
-    return shipReturn;
-  }
-
-  placeShip(ship) {
-    let placeAt = null;
-    if (this.isVerticalPlace == true) {
-      placeAt = y;
-    } else {
-      placeAt = x;
-    }
-  }
-
-  receiveAttack(x, y) {
-    if (x < 0 || y < 0 || x > this.boardSize[0] || y > this.boardSize[1]) {
-      return null;
-    }
-    if (checkCoordsForHit(x, y)) {
-      hitShip();
-    }
-  }
-
-  checkCoordsForHit(x, y) {
-    if (this.x[x] && this.y[y] == 1) {
-      return null;
-    }
-    if (this.x[x] && this.y[y] == 0) {
-      this.markPosition(x, y);
-      return null;
-    }
-    if (this.x[x] && this.y[y] == typeof Object) {
-      this.markAsHit(x, y);
+  checkFleetPlace() {
+    if (this.availableShips.length == 0) {
       return true;
+    } else {
+      return false;
     }
   }
 
-  markPosition(x, y) {
-    this.x[x] = 1;
-    this.y[y] = 1;
+  inBounds(row, column, ship) {
+    const shipLen = ship.length;
+    const finalRow = this.isVerticalPlace ? row + shipLen : row;
+    const finalColumn = this.isVerticalPlace ? column : column + shipLen;
+    if (finalRow <= this.boardSize[0] && finalColumn <= this.boardSize[1]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  placeShip(ship, row, column) {
+    const isEmpty = this.checkIfPositionEmpty(row, column);
+    const inBounds = this.inBounds(row, column, ship);
+    if (isEmpty && inBounds) {
+      this.removeFromFleet(ship);
+      this.updateBoardWithShip(ship, row, column);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  updateBoardWithShip(ship, row, column) {
+    const shipLen = ship.length;
+    const isVertical = this.isVerticalPlace;
+
+    if (isVertical) {
+      for (let index = 0; index < shipLen; index++) {
+        this.board[row + index][column] = ship;
+      }
+    } else {
+      for (let index = 0; index < shipLen; index++) {
+        this.board[row][column + index] = ship;
+      }
+    }
+  }
+
+  removeFromFleet(ship) {
+    const index = this.availableShips.indexOf(ship);
+    const placed = this.availableShips.splice(index, 1);
+    this.placedShips.push(placed);
+  }
+
+  moveToSunk(ship) {
+    const index = this.placedShips.indexOf(ship);
+    const sunk = this.availableShips.splice(index, 1);
+    this.sunkShips.push(sunk);
+  }
+
+  receiveAttack(row, column) {
+    const hitCheck = this.checkForHit(row, column);
+    if (hitCheck == false) {
+      this.markPosition(row, column);
+    }
+    if (typeof hitCheck == "object") {
+      this.shipWasHit(hitCheck);
+      this.markHit(row, column);
+    }
+    if (hitCheck == null) {
+      return null;
+    }
+  }
+
+  shipWasHit(ship) {
+    ship.hit();
+    this.isShipSunk(ship);
+  }
+
+  isShipSunk(ship) {
+    if (ship.isSunk()) {
+      this.moveToSunk(ship);
+    }
+  }
+
+  checkForHit(row, column) {
+    if (this.board[row][column] == 1) {
+      return null;
+    }
+    if (this.board[row][column] == 0) {
+      return false;
+    }
+    if (typeof this.board[row][column] == "object") {
+      const ship = this.board[row][column];
+      return ship;
+    }
+  }
+
+  markPosition(row, column) {
+    this.board[row][column] = 1;
+  }
+
+  markHit(row, column) {
+    this.board[row][column] = 2;
   }
 }
--- src/testing/gameboard.test.js
+++ src/testing/gameboard.test.js
@@ -3,21 +3,27 @@ import Ship from "../modules/Ship";
 const testGameboard = new Gameboard();
 
 const carrier = new Ship("carrier", 4);
-const battleship = new Ship("battleship", 3);
-const cruiser = new Ship("cruiser", 2);
-const submarine = new Ship("submarine", 1);
+const battleshipOne = new Ship("battleshipOne", 3);
+const battleshipTwo = new Ship("battleshipTwo", 3);
+const cruiserOne = new Ship("cruiserOne", 2);
+const cruiserTwo = new Ship("cruiserTwo", 2);
+const cruiserThree = new Ship("cruiserThree", 2);
+const submarineOne = new Ship("submarineOne", 1);
+const submarineTwo = new Ship("submarineTwo", 1);
+const submarineThree = new Ship("submarineThree", 1);
+const submarineFour = new Ship("submarineFour", 1);
 
 const shipFleet = [
   carrier,
-  battleship,
-  battleship,
-  cruiser,
-  cruiser,
-  cruiser,
-  submarine,
-  submarine,
-  submarine,
-  submarine,
+  battleshipOne,
+  battleshipTwo,
+  cruiserOne,
+  cruiserTwo,
+  cruiserThree,
+  submarineOne,
+  submarineTwo,
+  submarineThree,
+  submarineFour,
 ];
 
 test("check if board is created correctly", () => {
@@ -35,8 +41,10 @@ test("check if board is created correctly", () => {
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     ],
-    ships: shipFleet,
     isVerticalPlace: false,
+    availableShips: shipFleet,
+    placedShips: [],
+    sunkShips: [],
   });
 });
 
@@ -51,4 +59,15 @@ test("checks if negative values throws", () => {
   expect(() => testGameboard.checkIfPositionEmpty(15, 1).toThrow(Error));
   expect(() => testGameboard.checkIfPositionEmpty(-20, 0).toThrow(Error));
   expect(() => testGameboard.checkIfPositionEmpty(35, -1).toThrow(Error));
+});
+
+test("checks if ship is not out of bounds", () => {
+  expect(testGameboard.inBounds(5, 0, submarineOne)).toBe(true);
+  expect(testGameboard.inBounds(0, 0, submarineOne)).toBe(true);
+  expect(testGameboard.inBounds(9, 9, submarineOne)).toBe(true);
+  expect(testGameboard.inBounds(0, 9, carrier)).toBe(false);
+  expect(testGameboard.inBounds(5, 1, carrier)).toBe(true);
+  testGameboard.isVerticalPlace = true;
+  expect(testGameboard.inBounds(9, 0, carrier)).toBe(false);
+  expect(testGameboard.inBounds(0, 9, carrier)).toBe(true);
 });
